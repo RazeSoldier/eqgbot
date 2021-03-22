3c86b463fbf692cec34152e121ffe90855aa650e
RazeSoldier <razesoldier@outlook.com>
2021-03-22 22:32:17 +0800

    重构GitTimeCommand

--- src/main/java/razesoldier/eqgbot/job/msghandler/GitTimeCommand.java
+++ src/main/java/razesoldier/eqgbot/job/msghandler/GitTimeCommand.java
@@ -2,8 +2,11 @@ package razesoldier.eqgbot.job.msghandler;
 
 import net.mamoe.mirai.message.GroupMessageEvent;
 import net.mamoe.mirai.message.data.At;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 class GitTimeCommand implements MessageHandler {
     private final GroupMessageEvent event;
@@ -17,24 +20,31 @@ class GitTimeCommand implements MessageHandler {
         //输入：.gittime ddhhmm
         //返回：yyyy-mm-dd hh:mm:ss
         try {
-            Date d = new Date();
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
             final var sender = event.getSender();
-            final var msg= event.getMessage().contentToString();
-            int dd = Integer.parseInt(msg.substring(9,11));
-            int hh = Integer.parseInt(msg.substring(11,13));
-            int mm = Integer.parseInt(msg.substring(13,15));
-            String text = df.format(new Date(d.getTime() + 8 * 60 * 60 * 1000L//+8时区
-                                                         + dd * 24 * 60 * 60 * 1000L//加上增强时间天数
-                                                         + hh * 60 * 60 * 1000L//加上增强时间小时数
-                                                         + mm * 60 * 1000L//加上增强时间分钟数
-                                            )
+            ZonedDateTime now = ZonedDateTime.now(ZoneId.of("+8")); // 当前的北京时间
+            var reply = String.format("当前时间：%s\n计算后的时间：%s",
+                    now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                    plusTime(now, event.getMessage().contentToString())
             );
             event.getGroup().sendMessage(
-                    new At(sender).plus(text)
+                    new At(sender).plus(reply)
             );
         } catch (Exception e) {
-
+            event.getGroup().sendMessage(e.getMessage());
         }
+    }
+
+    @NotNull
+    private static String plusTime(ZonedDateTime baseTime, String plusText) throws Exception {
+        plusText = plusText.split(" ")[1];
+        if (plusText.length() != 6) {
+            throw new Exception("格式必须为ddhhmm（dd为天数，含前导零；hh为小时数；mm为分钟数）");
+        }
+
+        return baseTime
+                .plusDays(Long.parseLong(plusText.substring(0, 2)))
+                .plusHours(Long.parseLong(plusText.substring(2, 4)))
+                .plusMinutes(Long.parseLong(plusText.substring(4, 6)))
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 }
