c7cdd8592c55f668dd8e7f1b1b2d7889cafdc3c6
RazeSoldier <razesoldier@outlook.com>
2022-06-30 00:42:00 +0800

    refactor: 使用lombok重构Config类，移除无聊的setter/getter

--- src/main/java/razesoldier/eqgbot/Config.java
+++ src/main/java/razesoldier/eqgbot/Config.java
@@ -9,10 +9,13 @@
 
 package razesoldier.eqgbot;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Data
 public class Config {
     private List<String> features = new ArrayList<>();
     private DatabaseConfig gfDatabaseConfig = new DatabaseConfig();
@@ -26,151 +29,17 @@ public class Config {
     private String structureAlertQueueKey;
     private List<Long> structureAlertGroups;
 
-    public List<Long> getPingGroupList() {
-        return pingGroupList;
-    }
-
-    public void setPingGroupList(List<Long> pingGroupList) {
-        this.pingGroupList = pingGroupList;
-    }
-
-    public List<String> getFeatures() {
-        return features;
-    }
-
-    public void setFeatures(List<String> features) {
-        this.features = features;
-    }
-
-    public DatabaseConfig getGfDatabaseConfig() {
-        return gfDatabaseConfig;
-    }
-
-    public void setGfDatabaseConfig(DatabaseConfig gfDatabaseConfig) {
-        this.gfDatabaseConfig = gfDatabaseConfig;
-    }
-
-    public QQAccount getAccount() {
-        return account;
-    }
-
-    public void setAccount(QQAccount account) {
-        this.account = account;
-    }
-
-    public void setSovAlertGroup(int sovAlertGroup) {
-        this.sovAlertGroup = sovAlertGroup;
-    }
-
-    public int getSovAlertGroup() {
-        return sovAlertGroup;
-    }
-
-    public String getSovAlertQueueKey() {
-        return sovAlertQueueKey;
-    }
-
-    public void setSovAlertQueueKey(String sovAlertQueueKey) {
-        this.sovAlertQueueKey = sovAlertQueueKey;
-    }
-
-    public Map<String, Integer> getPingGroups() {
-        return pingGroups;
-    }
-
-    public void setPingGroups(Map<String, Integer> pingGroups) {
-        this.pingGroups = pingGroups;
-    }
-
-    public String getRedisPassword() {
-        return redisPassword;
-    }
-
-    public void setRedisPassword(String redisPassword) {
-        this.redisPassword = redisPassword;
-    }
-
-    public List<Long> getVettedGroupList() {
-        return vettedGroupList;
-    }
-
-    public void setVettedGroupList(List<Long> vettedGroupList) {
-        this.vettedGroupList = vettedGroupList;
-    }
-
-    public String getStructureAlertQueueKey() {
-        return structureAlertQueueKey;
-    }
-
-    public void setStructureAlertQueueKey(String structureAlertQueueKey) {
-        this.structureAlertQueueKey = structureAlertQueueKey;
-    }
-
-    public List<Long> getStructureAlertGroups() {
-        return structureAlertGroups;
-    }
-
-    public void setStructureAlertGroups(List<Long> structureAlertGroups) {
-        this.structureAlertGroups = structureAlertGroups;
-    }
-
+    @Data
     public static class DatabaseConfig {
         private String serverName;
         private String databaseName;
         private String user;
         private String password;
-
-        public String getServerName() {
-            return serverName;
-        }
-
-        public void setServerName(String serverName) {
-            this.serverName = serverName;
-        }
-
-        public String getDatabaseName() {
-            return databaseName;
-        }
-
-        public void setDatabaseName(String databaseName) {
-            this.databaseName = databaseName;
-        }
-
-        public String getUser() {
-            return user;
-        }
-
-        public void setUser(String user) {
-            this.user = user;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
     }
 
+    @Data
     public static class QQAccount {
         private long id;
         private String password;
-
-        public long getId() {
-            return id;
-        }
-
-        public void setId(long id) {
-            this.id = id;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
     }
 }
