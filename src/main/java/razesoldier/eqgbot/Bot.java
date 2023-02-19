5b65e2e440e2152e5f3557d8d36d36fcb0e75cd7
RazeSoldier <razesoldier@outlook.com>
2023-02-20 01:08:37 +0800

    使用fastjson2

--- src/main/java/razesoldier/eqgbot/Bot.java
+++ src/main/java/razesoldier/eqgbot/Bot.java
@@ -9,7 +9,7 @@
 
 package razesoldier.eqgbot;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import net.mamoe.mirai.BotFactory;
 import net.mamoe.mirai.utils.BotConfiguration;
 import net.mamoe.mirai.utils.MiraiLogger;
